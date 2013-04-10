c974137f00ed4d3adf38f7300c185196a0e10055
cadangelo <cadangelo@wisc.edu>
2013-04-10 12:05:45 -0500

    previous el nums were wrong, these are correct

--- fix.py
+++ fix.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
-# print to screen
-
-
 import os
 import sys
 import math
@@ -11,9 +8,9 @@ import linecache
 
 print 'hello python'
   
-def splitbycomma(stringy):
+def splitbycomma(line):
       nodes=[]
-      firstline=stringy.split(',',1)
+      firstline=line.split(',',1)
       el_num = firstline[0]
       remainder=firstline[1]
       for i in range(1,4):
@@ -30,9 +27,6 @@ f = open('sr_mesh220.inp','r')
 # open edited file
 g = open('fixed.inp', 'w')
 
-
-lines=[line for line in f.readlines()]
-f.seek(0)
 flag=0
 val=0 
 i=0
@@ -45,26 +39,23 @@ for line in f:
 #     i=i+1
      if '*ELEMENT' in line:  #If *ELEMENT is in the line, 
           flag=1
-	  #print i,line
      if '**\n' in line:
           val=0
 
      if ( flag == 1):
 	if '*ELEMENT' not in line: 
-#          splitLine=lines[i].split(',',1)
 	  splitLine=line.split(',',1)
           firstElNum=int(splitLine[0])
-#	  print firstElNum
 	  list_firstEl.append(firstElNum)
 	  firstEl=list_firstEl[-1]
 	  flag=0
 	  val=1
 
      if  ( val == 1 and flag == 0 ):
-	(bob,geldof)=splitbycomma(line)
-	list_element.append(bob)
-	list_node.append(geldof)
-#	print list_element
+	(el_num,nodes)=splitbycomma(line)
+	list_element.append(el_num)
+	list_node.append(nodes)
+#	print list_element[-1]
 #	print list_firstEl[0]
 
 #	print newEl
@@ -89,8 +80,9 @@ for l in range(0,len(list_element)):
         else:
             j=j
      # new value is the original value - start value + 1
-     newEl=(int(list_element[l])-int(list_firstEl[j-1]) + 1)
+     newEl=(int(list_element[l])-int(list_firstEl[j]) + 1)
      new_element.append(newEl)
+     
 
 # loop over the members of list_element
 for l in range(0,len(list_element)):
@@ -99,16 +91,16 @@ for l in range(0,len(list_element)):
 	# if we are not on the final elset increment j
 	if j < len(list_firstEl)-1:
 	    j=j+1
-	    print "*ELSET=EB"
+#	    print "*ELSET=EB"
 	# otherwise do nothing
         else:
             j=j
-	    print "*ELSET=EB"
+#	    print "*ELSET=EB"
      # new value is the original value - start value + 1
-     print new_element[l],list_node[l]
+     print new_element[l]#,list_node[l]
+#     g.write(str(new_element))	
 
-
-#Sprint list_element[-1] #,list_node
+#print list_element[-1] #,list_node
 #print list_element[-1],list_node[-1]
 
 
